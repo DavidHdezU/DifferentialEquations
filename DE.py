f7fecbe0fd7eee79d410b7b7a7fa5cedfcd5d9d6
DavidHdezU <Dhdezu@ciencias.unam.mx>
2021-08-19 22:11:09 -0500

    Adding Lineal Systems pt.1

--- DE.py
+++ DE.py
@@ -1,6 +1,5 @@
 from scipy.integrate import odeint
 import numpy as np
-import matplotlib.pyplot as plt
 
 def predator_prey_model(x, t, a, b, c, d):
     # Assign each ODE to a element from the list x
@@ -13,6 +12,16 @@ def predator_prey_model(x, t, a, b, c, d):
     
     return [dx_dt, dy_dt]
 
+def lineal_system(y, t, x1, y1, x2, y2):
+    initial_x = y[0]
+    initial_y = y[1]
+    
+    dx_dt = x1*initial_x + y1*initial_y
+    dy_dt = x2*initial_x + y2*initial_y
+    
+    return [dx_dt, dy_dt]
+    
+    
 
 
 
